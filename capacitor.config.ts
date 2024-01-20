43b9062933b34ce8907dbf91c8a9061b69ded6ef
hannah_huang <hannah_huang@paradise-soft.com.tw>
2024-01-20 17:33:31 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AI_Conversation_APP',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
